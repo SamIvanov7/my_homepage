018e76bb764cbc81884c58c3fe61a6ff709c6d81
Sam <sam1206morfey@gmail.com>
2022-08-29 13:41:26 +0300

    my new homepage

---
+++ js/app2.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 0.9,
+  scaleMobile: 1.00,
+  color: 0x3fffdd,
+  color2: 0x86f50a,
+  size: 1.2,
+  backgroundColor: 0x9000c
+})
